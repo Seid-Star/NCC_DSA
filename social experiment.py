a01fc390532cdbd957f4c905174c3549fe0c817d
Seid-Star <sjrd02911@gmail.com>
2026-01-20 09:23:41 +0300

    Add social experiment script for number classification

---
+++ social experiment.py
@@ -0,0 +1,11 @@
+a=int(input())
+for x in range(a):
+    b=int(input())
+    if b==2:
+        print(2)
+    elif b==3:
+        print(3)
+    elif b%2==0:
+        print(0)
+    else:
+        print(1)
