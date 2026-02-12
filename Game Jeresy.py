f048647e6f2608aa8d57bce557f2181cbf76cf72
Seid-Star <sjrd02911@gmail.com>
2026-02-12 17:30:14 +0300

    Create Game Jeresy.py

---
+++ Game Jeresy.py
@@ -0,0 +1,12 @@
+a=int(input())
+arr=[]
+brr=[]
+for x in range(a):
+    b,c=map(int,input().split())
+    arr.append(b)
+    brr.append(c)
+count=0
+for i in range(a):
+    d=brr.count(arr[i])
+    count=count+d
+print(count)
