9f2a895ccacf830b82ededf772ae4483fc2c5a50
Seid-Star <sjrd02911@gmail.com>
2026-02-17 14:48:12 +0300

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,21 @@
+a=int(input())
+for x in range(a):
+    b=int(input())
+    if b>=1900:
+        print("Division 1")
+    elif 1600<=b<=1899:
+        print("Division 2")
+    elif 1400<=b<=1599:
+        print("Division 3")
+    else:
+        print("Division 4")
+
+            
+
+            
+                
+            
+            
+        
+        
+
